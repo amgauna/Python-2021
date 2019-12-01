d1dfcb28059cf0bd07a5ab00ddb8a58aeb226b4f
Ana Gauna <amgauna@gmail.com>
2019-12-01 13:53:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,28 @@ setup(name='Distutils',
       packages=['distutils', 'distutils.command'],
      )
 
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-AnaGauna", # Replace with your own username
+    version="0.0.1",
+    author="Ana Gauna",
+    author_email="amgauna@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amgauna/Python/.github/workflows/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
 
 
 
