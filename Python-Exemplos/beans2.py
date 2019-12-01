1d0397ae32998eb22a29f7503126e7ca8fa0185a
Ana Gauna <amgauna@gmail.com>
2019-12-01 05:17:21 -0300

    Update beans2.py

--- Python-Exemplos/beans2.py
+++ Python-Exemplos/beans2.py
@@ -8,7 +8,7 @@ while price > 4.74:
       where = text.find('>$')
       start_of_price = where + 2
       end_of_pricwe = start_of_price + 4
-      return float(text(start_of_price:end_of_price))
+      return float(text(start_of_price, end_of_price))
 price_now = input("Do you want to see the price now (y/n)? ")
 if price_now = "y":
    print(get_price())
