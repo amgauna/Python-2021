240bf52369e895ee92f0c0b99a6c3452c714b93d
Ana Gauna <amgauna@gmail.com>
2019-12-01 05:35:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-pip-review --auto
+pip-review auto
 make install-pip-requirements 
-pip install --upgrade --force-reinstall -r requirements.txt
+pip install upgrade force-reinstall -r requirements.txt
 pip freeze > requirements.txt
 cat requirements.txt
-pip install --user --requirement requirements.txt
+pip install user requirement requirements.txt
 
 
 
