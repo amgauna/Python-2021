ae7780eb9716e9479b2f5f8060d1751fc9bc213a
Ana Gauna <amgauna@gmail.com>
2019-12-01 13:47:20 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-$ pip freeze > requirements.txt
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='3.8',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
 
 
 
