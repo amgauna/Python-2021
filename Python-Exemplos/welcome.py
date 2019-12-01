57a992ac5f52dd10f7a4ebc0edc05d7ba5025cb9
Ana Gauna <amgauna@gmail.com>
2019-12-01 05:34:12 -0300

    Create welcome.py

---
+++ Python-Exemplos/welcome.py
@@ -0,0 +1,13 @@
+print ("Welcome!")
+guess = 0
+while guess != 5:
+    g = input("Guess the number: ")
+    guess = int(g)
+    if guess == 5:
+       print("You win!")
+    else:
+        if guess > 5:
+           print("Too high")
+        else:
+           print ("You low!")
+print ("Game over!")
