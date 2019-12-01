b79251267890b34c3ba6eeca64ca80b9213b18bd
Ana Gauna <amgauna@gmail.com>
2019-12-01 14:54:26 -0300

    Update activate.fish

--- activate.fish
+++ activate.fish
@@ -1,9 +1,9 @@
 $ source ~/envs/tutorial-env/bin/activate
 (tutorial-env) $ python
-Python 3.5.1 (default, May  6 2016, 10:59:36)
+Python 3.7.1 (default, May  6 2018, 10:59:36)
   ...
 >>> import sys
 >>> sys.path
-['', '/usr/local/lib/python35.zip', ...,
+['', '/usr/local/lib/python37.zip', ...,
 '~/envs/tutorial-env/lib/python3.5/site-packages']
 >>>
