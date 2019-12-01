0cf62ad596cd682dbe3b27b44b20f30ec8be7cbe
Ana Gauna <amgauna@gmail.com>
2019-11-30 22:00:53 -0300

    Update starbuzz.py

--- Python-Exemplos/starbuzz.py
+++ Python-Exemplos/starbuzz.py
@@ -2,3 +2,9 @@ import urllib.request
 page = urllib.request.urlopen("http://www.beans-r-us.biz/prices.html")
 text = page.read().decode("utf8")
 print(text)
+
+where = text.find('>$')
+start_of_price = where + 2
+end_of_pricwe = start_of_price + 4
+price = text(start_of_price:end_of_price)
+print(price)
