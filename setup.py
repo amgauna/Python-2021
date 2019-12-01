ba7214e8d5416ab9815ff8f7e813f3fc44868562
Ana Gauna <amgauna@gmail.com>
2019-12-01 04:58:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-$ pip install --upgrade requests
-$ pip install pip-upgrader
-$ pip install flask
-$ pip install flake8
-$ pip-review --auto
-$ make install-pip-requirements 
-$ pip install --upgrade --force-reinstall -r requirements.txt
-$ pip freeze > requirements.txt
-$ cat requirements.txt
-env_2)$ pip install --user --requirement requirements.txt
+pip install requests
+pip install --upgrade requests
+pip install pip-upgrader
+pip install flask
+pip install flake8
+pip-review --auto
+make install-pip-requirements 
+pip install --upgrade --force-reinstall -r requirements.txt
+pip freeze > requirements.txt
+cat requirements.txt
+pip install --user --requirement requirements.txt
 
 
 
