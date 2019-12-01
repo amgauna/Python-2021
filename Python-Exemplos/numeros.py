5bbb0c0bd4cce1b5d1602192527af27c8cba920f
Ana Gauna <amgauna@gmail.com>
2019-12-01 05:59:29 -0300

    Update numeros.py

--- Python-Exemplos/numeros.py
+++ Python-Exemplos/numeros.py
@@ -1,39 +1,39 @@
->>> # Utilizando Python Como Uma Calculadora
->>> # Vamos tentar alguns comandos simples em Python. 
->>> # Inicie o interpretador e aguarde o prompt primário, ">>> ". (Não deve demorar muito.)
+# Utilizando Python Como Uma Calculadora
+# Vamos tentar alguns comandos simples em Python. 
+# Inicie o interpretador e aguarde o prompt primário, ">>> ". (Não deve demorar muito.)
 
->>> # 3.1.1 Números
->>> # O interpretador atua como uma calculadora bem simples: você pode digitar uma expressão e o valor resultando será
->>> # apresentado após a avaliação da expressão. A sintaxe da expressão é a usual: operadores +, -, * e / funcionam da
->>> # mesma forma que em outras linguagens tradicionais (por exemplo, Pascal ou C); parênteses podem ser usados para 
->>> # definir agrupamentos. 
+# 3.1.1 Números
+# O interpretador atua como uma calculadora bem simples: você pode digitar uma expressão e o valor resultando será
+# apresentado após a avaliação da expressão. A sintaxe da expressão é a usual: operadores +, -, * e / funcionam da
+# mesma forma que em outras linguagens tradicionais (por exemplo, Pascal ou C); parênteses podem ser usados para 
+# definir agrupamentos. 
 
->>> # Por exemplo:
+# Por exemplo:
 
 >>> 2+2
 4
->>> # Isso é um comentário
+# Isso é um comentário
 ... 2+2
 4
 >>> 2+2  # e um comentário na mesma linha de um comando
 4
 >>> (50-5*6)/4
 5
->>> # Divisão inteira retorna com aredondamento para base
+# Divisão inteira retorna com aredondamento para base
 ... 7/3
 2
 >>> 7/-3
 -3
 
->>> # O sinal de igual ("=") é utilizado para atribuição de um valor a uma variável. 
->>> # Nenhum resultado é exibido até o próximo prompt interativo:
+# O sinal de igual ("=") é utilizado para atribuição de um valor a uma variável. 
+# Nenhum resultado é exibido até o próximo prompt interativo:
 
 >>> width = 20
 >>> height = 5*9
 >>> width * height
 900
 
->>> # Um valor pode ser atribuído a diversas variáveis simultaneamente:
+# Um valor pode ser atribuído a diversas variáveis simultaneamente:
 
 >>> x = y = z = 0  # Zero x, y e z
 >>> x
@@ -43,16 +43,16 @@
 >>> z
 0
 
->>> # Há total suporte para ponto-flutuante; operadores com operandos de diferentes tipos convertem o inteiro para ponto-flutuante:
+# Há total suporte para ponto-flutuante; operadores com operandos de diferentes tipos convertem o inteiro para ponto-flutuante:
 
 >>> 3 * 3.75 / 1.5
 7.5
 >>> 7.0 / 2
 3.5
 
->>> # Números complexos também são suportados; números imaginários são escritos com o sufixo "j" ou "J". 
->>> # Números complexos com parte real não nula são escritos como "(real+imagj)", ou podem ser criados 
->>> # pela chamada de função "complex(real, imag)".
+# Números complexos também são suportados; números imaginários são escritos com o sufixo "j" ou "J". 
+# Números complexos com parte real não nula são escritos como "(real+imagj)", ou podem ser criados 
+# pela chamada de função "complex(real, imag)".
 
 >>> 1j * 1J
 (-1+0j)
@@ -65,8 +65,8 @@
 >>> (1+2j)/(1+1j)
 (1.5+0.5j)
 
->>> # Números complexos são sempre representados por dois números ponto-flutuante, a parte real e a parte imaginária. 
->>> # Para extrair as partes de um número z, utilize z.real e z.imag.
+# Números complexos são sempre representados por dois números ponto-flutuante, a parte real e a parte imaginária. 
+# Para extrair as partes de um número z, utilize z.real e z.imag.
 
 >>> a=1.5+0.5j
 >>> a.real
@@ -74,9 +74,9 @@
 >>> a.imag
 0.5
 
->>> # As funções de conversão para ponto-flutuante e inteiro (float(), int() e long()) não funcionam para números complexos 
->>> # -- não existe maneira correta de converter um número complexo para um número real. 
->>> # Utilize abs(z) para obter sua magnitude (como ponto-flutuante) ou z.real para obter sua parte real.
+# As funções de conversão para ponto-flutuante e inteiro (float(), int() e long()) não funcionam para números complexos 
+# -- não existe maneira correta de converter um número complexo para um número real. 
+# Utilize abs(z) para obter sua magnitude (como ponto-flutuante) ou z.real para obter sua parte real.
 
 >>> a=3.0+4.0j
 >>> float(a)
@@ -90,8 +90,8 @@ TypeError: can't convert complex to float; use abs(z)
 >>> abs(a)
 5.0
 
->>> # No modo interativo, a última expressão a ser impressa é atribuída a variável _.  Isso significa que ao utilizar
->>> # Python como uma calculadora, é muitas vezes mais fácil prosseguir com os cálculos da seguinte forma:
+# No modo interativo, a última expressão a ser impressa é atribuída a variável _.  Isso significa que ao utilizar
+# Python como uma calculadora, é muitas vezes mais fácil prosseguir com os cálculos da seguinte forma:
 
 >>> tax = 12.5 / 100
 >>> price = 100.50
@@ -101,6 +101,6 @@ TypeError: can't convert complex to float; use abs(z)
 113.0625
 >>> round(_, 2)
 113.06
->>> # Essa variável especial deve ser tratada somente para leitura pelo usuário. Nunca lhe atribua explicitamente um valor 
->>> # - do contrário, estaria se criando uma outra variável (homônima) independente, que mascararia o comportamento mágico 
->>> # da variável especial.
+# Essa variável especial deve ser tratada somente para leitura pelo usuário. Nunca lhe atribua explicitamente um valor 
+# - do contrário, estaria se criando uma outra variável (homônima) independente, que mascararia o comportamento mágico 
+# da variável especial.
