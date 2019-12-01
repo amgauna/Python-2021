9a8d24c2479a13591df9495036cafc1d70b45a3d
Ana Gauna <amgauna@gmail.com>
2019-12-01 05:01:09 -0300

    Create setup.py

---
+++ .github/workflows/setup.py
@@ -0,0 +1,11 @@
+pip install requests
+pip install --upgrade requests
+pip install pip-upgrader
+pip install flask
+pip install flake8
+pip-review --auto
+make install-pip-requirements 
+pip install --upgrade --force-reinstall -r requirements.txt
+pip freeze > requirements.txt
+cat requirements.txt
+pip install --user --requirement requirements.txt
