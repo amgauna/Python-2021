99fda9996d162882ab38478fc3dcd57e6a31e989
Ana Gauna <amgauna@gmail.com>
2019-12-01 04:42:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,13 @@ $ pip install --upgrade requests
 $ pip install pip-upgrader
 $ pip install --upgrade --force-reinstall -r requirements.txt
 $ pip-review --auto
+
+pip install flake8
+flake8 --version
+3.5.0 (mccabe: 0.6.1, pycodestyle: 2.3.1, pyflakes: 1.5.0) CPython 2.7.14 on Darwin
+
+pip3 install flake8
+python3 -m flake8 --version
+3.5.0 (mccabe: 0.6.1, pycodestyle: 2.3.1, pyflakes: 1.6.0) CPython 3.6.1 on Darwin
+python3 -m flake8 --exclude migrations --max-line-length=121
+
