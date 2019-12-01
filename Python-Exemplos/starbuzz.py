b594f063d3f39e639ff3362dff29f4e15130fe7a
Ana Gauna <amgauna@gmail.com>
2019-12-01 05:23:17 -0300

    Update starbuzz.py

--- Python-Exemplos/starbuzz.py
+++ Python-Exemplos/starbuzz.py
@@ -5,6 +5,6 @@ print(text)
 
 where = text.find('>$')
 start_of_price = where + 2
-end_of_pricwe = start_of_price + 4
-price = text(start_of_price.end_of_price)
+end_of_price = start_of_price + 4
+price = text(start_of_price:end_of_price)
 print(price)
