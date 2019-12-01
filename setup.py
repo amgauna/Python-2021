320123444f59b5898d17098a783bb696a463f526
Ana Gauna <amgauna@gmail.com>
2019-12-01 16:04:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ import sys
 import time
 import pytest
 import unittest
+import os
 
+from setuptools import setup
 from pytest import ExitCode
 from selenium import webdriver
 from pages import *
@@ -13,19 +15,42 @@ from locators import *
 from selenium.webdriver.common.by import By
 from distutils.core import setup
 
-class RegisterNewInstructor(unittest.TestCase):
-      
-setup(name='Distutils',
-      version='3.x',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-     )
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def pytest_load_initial_conftests(args):
+    if "xdist" in sys.modules:  # pytest-xdist plugin
+        import multiprocessing
+
+        num = max(multiprocessing.cpu_count() / 2, 1)
+        args[:] = ["-n", str(num)] + args
+            
+def test_placeholder():
+    pass
+
+setup(
+    name = "an_example_pypi_project",
+    version = "0.0.4",
+    author = "Andrew Carter",
+    author_email = "andrewjcarter@gmail.com",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=['an_example_pypi_project', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
 
 setuptools.setup(
     name="example-pkg-AnaGauna", # Replace with your own username
@@ -44,16 +69,6 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
 )
-
-def pytest_load_initial_conftests(args):
-    if "xdist" in sys.modules:  # pytest-xdist plugin
-        import multiprocessing
-
-        num = max(multiprocessing.cpu_count() / 2, 1)
-        args[:] = ["-n", str(num)] + args
-            
-def test_placeholder():
-    pass
 
 
 
