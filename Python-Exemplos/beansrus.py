541bad8e163c87989cc047e9684b3d4992738a7c
Ana Gauna <amgauna@gmail.com>
2019-12-01 05:24:46 -0300

    Update beansrus.py

--- Python-Exemplos/beansrus.py
+++ Python-Exemplos/beansrus.py
@@ -5,6 +5,6 @@ while price > 4.74:
       text = page.read().decode("utf8")
       where = text.find('>$')
       start_of_price = where + 2
-      end_of_pricwe = start_of_price + 4
-      price = float(text(start_of_price, end_of_price))
+      end_of_price = start_of_price + 4
+      price = float(text(start_of_price:end_of_price))
 print("Buy!")
