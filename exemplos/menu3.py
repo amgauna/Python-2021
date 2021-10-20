60311c50a100d1b081f57c16a76f44973e51e8ee
Ana Gauna <amgauna@gmail.com>
2021-10-19 22:05:11 -0300

    Create menu3.py

---
+++ exemplos/menu3.py
@@ -0,0 +1,17 @@
+def menu():
+    print('''
+            MENU:
+
+            [S] - Serviços
+            [P] - Portifólio
+            [E] - Sobre Mim
+            [C] - Contato
+            [S] - Sair
+        ''')
+    str(input('Escolha uma opção: '))
+
+verServicos = "s"
+verPortifolio =  "p"
+verSobreMim = "e"
+verContato = "c"
+sair = "s"
