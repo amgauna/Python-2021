fc61b3a4f261f129e8a161469d6a97377595d032
Ana Gauna <amgauna@gmail.com>
2021-10-20 14:43:13 -0300

    Delete activate.fish

--- activate.fish
+++
@@ -1,9 +0,0 @@
-$ source ~/envs/tutorial-env/bin/activate
-(tutorial-env) $ python
-Python 3.7.1 (default, May  6 2018, 10:59:36)
-  ...
->>> import sys
->>> sys.path
-['', '/usr/local/lib/python37.zip', ...,
-'~/envs/tutorial-env/lib/python3.5/site-packages']
->>>
