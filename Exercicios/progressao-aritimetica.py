1ab07fcf7d7d23c58626f10b87a4a29a0cb59701
Ana Gauna <amgauna@gmail.com>
2021-10-20 02:29:28 -0300

    Create progressao-aritimetica.py

---
+++ Exercicios/progressao-aritimetica.py
@@ -0,0 +1,9 @@
+print ("Progressão Aritimética")
+print (" "*50)
+a1 = int(input("Digite o primeiro termo da sucessão a1:"))
+d = int(input("Digite a razão d:"))
+n = int(input("Digite o número do termo que quer encontrar:))
+print (" "*50)
+s = a1+(n-1)*d
+print ("o valor de a ",n, " é ",s)
+              
