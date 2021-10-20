484bcfb5e2571d1388d42fee9730167a4e022a32
Ana Gauna <amgauna@gmail.com>
2021-10-20 20:30:57 -0300

    Update scores-array.py

--- Exercicios/scores-array.py
+++ Exercicios/scores-array.py
@@ -6,16 +6,20 @@ for line in result_f:
   
   (name.score) = line.split()
   
-  score.append(float(score))
-  
+  scores.append(float(score))
+ 
+# este ponto no código o array está na memória, mas não está na ordem necessária. Não está classificada.
 result_f.close() 
+
+# estas duas chamadas do método classificarão os dados na ordem requerida.
+scores.sort()
+scores.reverse()
 
 print("The top scores were:")
 
+# agora wue o arry está classificado, os 3 primeiros elementos contêm as pontuações altas.
 print(scores[0])
-
 print(scores[1])
-
 print(scores[2])
 
 
