c16caed3f40de7b24a1229eb2571182581ef7227
Ana Gauna <amgauna@gmail.com>
2021-10-20 04:18:26 -0300

    Create idade.py

---
+++ Exercicios/idade.py
@@ -0,0 +1,17 @@
+
+# idade
+
+nome = input("Digite seu nome: ")
+
+idade = input("Digite sua idade: ")
+
+data = input("Digite o dia do seu aniversário: ")
+
+mes = input("Digite o mês do seu aniversário: ")
+
+ano = input("digite o ano do seu aniversário: ")
+
+print ("Olá {} seja bem vindo(a)!!!.format(nome))
+
+print ("Você tem {ano} anos, e nasceu em {data} de {mes} de {ano}.format(idade,data,mes,ano))
+       
