58cf64f9531e0cacb9f0da2560306ce84916aa63
Ana Gauna <amgauna@gmail.com>
2021-10-20 04:19:56 -0300

    Update idade.py

--- Exercicios/idade.py
+++ Exercicios/idade.py
@@ -11,7 +11,7 @@ mes = input("Digite o mês do seu aniversário: ")
 
 ano = input("digite o ano do seu aniversário: ")
 
-print ("Olá {} seja bem vindo(a)!!!.format(nome))
+print ("Olá {nome} seja bem vindo(a)!!!.format(nome))
 
 print ("Você tem {ano} anos, e nasceu em {data} de {mes} de {ano}.format(idade,data,mes,ano))
        
