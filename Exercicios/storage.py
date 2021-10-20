8d60728efdaa3c81553a4adf97f4f0c9e5285048
Ana Gauna <amgauna@gmail.com>
2021-10-20 03:50:38 -0300

    Create storage.py

---
+++ Exercicios/storage.py
@@ -0,0 +1,30 @@
+# if a system has a block size of 4096 bytes, this means that a file comprised of only one byte will still use 4096 bytes of
+# storage. A file made up of 4097 bytes will use 4096*2=8192 bytes of storage. Knowing this, can you fill in the gaps in the
+# calculate_storage function below. Which calculates the total number of baytes needed to store a file of a given size?
+
+# calculate storage
+
+def calculate_storage(filesize)
+    block_size = 4096
+	
+	# Use floor division to calculate how many blocks are fully ocupied
+	full_blocks = filesize//block_size
+	
+	# Use the modulo operator to check whether there´s any remainder
+	partial_block_remainder = full_block%2
+	
+	# Depending on whether there´s a remainder or not, return
+	# The totalnumber of bytes required to allocad enought blocks
+	# to store your data.
+	
+	if patial_block_remainder > 0:
+	   return 2
+	   
+return 2
+
+print (calculate_storage(1)) # should be 4096	
+print (calculate_storage(4096)) # should be 4096
+print (calculate_storage(4097)) # should be 8192
+print (calculate_storage(6000)) # should be 8192   
+	
+	
