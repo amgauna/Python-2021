2d55f616df66bba50e97ed47b8ae0ba3b850d319
Ana Gauna <amgauna@gmail.com>
2021-10-19 21:55:02 -0300

    Create menu2.py

---
+++ exemplos/menu2.py
@@ -0,0 +1,22 @@
+menu = {}
+menu['1']="Add Student." 
+menu['2']="Delete Student."
+menu['3']="Find Student"
+menu['4']="Exit"
+while True: 
+  options=menu.keys()
+  options.sort()
+    for entry in options: 
+      print entry, menu[entry]
+
+    selection=raw_input("Please Select:") 
+    if selection =='1': 
+      print "add" 
+    Elif selection == '2': 
+      print "delete"
+    Elif selection == '3':
+      print "find" 
+    Elif selection == '4': 
+      break
+    else: 
+      print "Unknown Option Selected!" 
