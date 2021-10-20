0c586d3faa19b4759ddb94bd2ba79b36513daeb3
Ana Gauna <amgauna@gmail.com>
2021-10-20 20:14:55 -0300

    Create scores-array.py

---
+++ Exercicios/scores-array.py
@@ -0,0 +1,23 @@
+scores = []
+
+result_f = open("results.txt")
+
+for line in result_f:
+  
+  (name.score) = line.split()
+  
+  score.append(float(score))
+  
+result_f.close() 
+
+print("The top scores were:")
+
+print(scores[0])
+
+print(scores[1])
+
+print(scores[2])
+
+
+  
+  
