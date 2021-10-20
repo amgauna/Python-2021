3a6123f729722f1a013f5eddc6c5c51819cb6838
Ana Gauna <amgauna@gmail.com>
2021-10-20 03:12:30 -0300

    Create concatenacao.py

---
+++ Exercicios/concatenacao.py
@@ -0,0 +1,10 @@
+# concatenação de string
+
+c1 = "nome:"
+c2 = "sobrenome:"
+c3 = "email:"
+c4 = "senha:"
+
+result = "\n" + c1 + "\n" + c2 + "\n" +c3 + "\n" + c4
+
+print("t\t\t Crie sua conta" + result.title())
