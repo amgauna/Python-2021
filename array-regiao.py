c43a9172a0570c1874044445ab607bb87a5e16d8
Ana Gauna <amgauna@gmail.com>
2021-12-05 18:00:31 -0300

    Update array-regiao.py

--- array-regiao.py
+++ array-regiao.py
@@ -4,22 +4,14 @@ sys.__stdin__
 
 import numpy as np
 
-# criando as listas
-titulo = ['CÓDIGO', 'INADIMPLÊNCIA', 'REGIÃO', 'LITROS/MÊS']
-codigo = [47798, 88333, 71928, 74830, 42536]
-inadimplencia = [0, 0, 0, 0, 0]
-regiao = [1, 2, 3, 4, 5]
-lista_regiao = ['regiao-centro', 'regiao_grande_ibis', 'regiao_grande_aribiri', 'regiao_grande_cobilandia', 'regiao_grande_jucu']
-litros_mes = [85, 106, 69, 88, 65]
-          
 # criando os arrays
-titulo_array = np.array(titulo)
-codigo_array = np.array(codigo)
-inadimplencia_array = np.array(inadimplencia)
-regiao_array = np.array(regiao)
-lista_regiao_array = np.array(lista_regiao)
-litros_mes_array = np.array(litros_mes)
+titulo = np.array(['CÓDIGO', 'INADIMPLÊNCIA', 'REGIÃO', 'LITROS/MÊS'])
+codigo = np.array([47798, 88333, 71928, 74830, 42536])
+inadimplencia = np.array([0, 0, 0, 0, 0])
+regiao = np.array([1, 2, 3, 4, 5])
+lista_regiao = np.array.['regiao-centro', 'regiao_grande_ibis', 'regiao_grande_aribiri', 'regiao_grande_cobilandia', 'regiao_grande_jucu']
+litros_mes = np.array[85, 106, 69, 88, 65]
 
 # exiba as mensagens bem formatadas na tela
-print({titulo_array}, {codigo_array}, {inadimplencia_array}, {regiao_array}, {lista_regiao_array}, {litros_mes_array} "\n" )
+print({titulo}, {codigo}, {inadimplencia}, {regiao}, {lista_regiao}, {litros_mes}, "\n" )
 
