ed0b12dba6349368dcde13dc34f48b9a8550fba5
Ana Gauna <amgauna@gmail.com>
2021-12-05 14:50:06 -0300

    Update array-regiao2.py

--- array-regiao2.py
+++ array-regiao2.py
@@ -1,21 +1,23 @@
 import sys
+sys.getfilesystemencoding() returns 'UTF-8'
+
 import numpy as np
 
 # criando as listas
-lista_array = ([ ['CÓDIGO', 'INADIMPLÊNCIA', 'REGIÃO', 'LITROS/MÊS'],
-                 [47798, 0, 1:'regiao-centro', 85],
-                 [88333, 0, 2:'regiao_grande_ibis', 106],
-                 [71928, 0, 3:'regiao_grande_aribiri', 69],
-                 [74830, 0, 4:'regiao_grande_cobilandia', 88],
-                 [42536, 0, 5:'regiao_grande_jucu', 65] 
-               ])
+lista_array = ArrayType( ('CÓDIGO', 'INADIMPLÊNCIA', 'REGIÃO', 'LITROS/MÊS'),
+                         (47798, 0, 1:'regiao-centro', 85),
+                         (88333, 0, 2:'regiao_grande_ibis', 106),
+                         (71928, 0, 3:'regiao_grande_aribiri', 69),
+                         (74830, 0, 4:'regiao_grande_cobilandia', 88),
+                         (42536, 0, 5:'regiao_grande_jucu', 65) 
+                        )
                  
 # criando os arrays
-cria_array = np.array.format(lista_array)
+cria_array = np.format(lista_array)
 
 # exiba as mensagens bem formatadas na tela
-for lista_array in sys.stdin:
-if 'Exit' == lista_array.rstrip():
-break
+for cria_array in sys.stdin:
+if 'Exit' == cria_array.rstrip():
+exit()
           
 print(cria_array)
