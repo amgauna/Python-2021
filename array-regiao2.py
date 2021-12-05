8c4041ace8527cfb3ca74900e32f5a90676201c0
Ana Gauna <amgauna@gmail.com>
2021-12-05 15:32:18 -0300

    Update array-regiao2.py

--- array-regiao2.py
+++ array-regiao2.py
@@ -2,19 +2,16 @@ import sys
 sys.version_info
 sys.__stdin__
 
-import fibo as fib
-fib.fib(500)
-
 import numpy as np
 
 # criando as listas
-lista_array = ArrayType( ('CÓDIGO', 'INADIMPLÊNCIA', 'REGIÃO', 'LITROS/MÊS'),
-                         (47798, 0, 1:'regiao-centro', 85),
-                         (88333, 0, 2:'regiao_grande_ibis', 106),
-                         (71928, 0, 3:'regiao_grande_aribiri', 69),
-                         (74830, 0, 4:'regiao_grande_cobilandia', 88),
-                         (42536, 0, 5:'regiao_grande_jucu', 65) 
-                        )
+lista_array = { ('CÓDIGO', 'INADIMPLÊNCIA', 'REGIÃO', 'LITROS/MÊS'),
+                (47798, 0, 1:'regiao-centro', 85),
+                (88333, 0, 2:'regiao_grande_ibis', 106),
+                (71928, 0, 3:'regiao_grande_aribiri', 69),
+                (74830, 0, 4:'regiao_grande_cobilandia', 88),
+                (42536, 0, 5:'regiao_grande_jucu', 65) 
+              }
                  
 # criando os arrays
 np.cria_array = format(lista_array)
