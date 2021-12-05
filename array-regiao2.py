9ae5190968f525b858c090c829f9d306264572d8
Ana Gauna <amgauna@gmail.com>
2021-12-05 16:04:09 -0300

    Update array-regiao2.py

--- array-regiao2.py
+++ array-regiao2.py
@@ -3,18 +3,21 @@ sys.version_info
 sys.__stdin__
 
 import numpy as np
+a = np.arange(15).reshape(3, 5)
+type(a) 
+<class 'numpy.ndarray'>
 
 # criando as listas
 lista_array = { ('CÓDIGO', 'INADIMPLÊNCIA', 'REGIÃO', 'LITROS/MÊS'),
-                (47798, 0, 1:'regiao-centro', 85),
-                (88333, 0, 2:'regiao_grande_ibis', 106),
-                (71928, 0, 3:'regiao_grande_aribiri', 69),
-                (74830, 0, 4:'regiao_grande_cobilandia', 88),
-                (42536, 0, 5:'regiao_grande_jucu', 65) 
-              }
+          (47798, 0, 1:'regiao-centro', 85),
+          (88333, 0, 2:'regiao_grande_ibis', 106),
+          (71928, 0, 3:'regiao_grande_aribiri', 69),
+          (74830, 0, 4:'regiao_grande_cobilandia', 88),
+          (42536, 0, 5:'regiao_grande_jucu', 65) 
+           }
                  
 # criando os arrays
-np.cria_array = format(lista_array)
+cria_array = np.cria_array.format(lista_array)
 
 # exiba as mensagens bem formatadas na tela
 for np.cria_array in sys.stdin:
